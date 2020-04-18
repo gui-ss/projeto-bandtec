e6302f54daec1b2a86b6bbe574f92ddc8e16d18d
Christian Raphael <christianraphael.1309@gmail.com>
2020-04-18 17:44:52 -0300

    Modal de Login adicionado

---
+++ Site/js/modal.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById('modal-login');
+
+function openModal () {
+    modal.classList.add('modal-active');
+}
+
+function closeModal() {
+    modal.classList.remove('modal-active');
+}
