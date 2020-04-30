da58ebfd5a5eba9e68a057042c2c20ecdccdf6ef
Christian Raphael <christianraphael.1309@gmail.com>
2020-04-30 16:54:42 -0300

    Estrutura de Pastas reorganizada

---
+++ web/sistema_web/js.js
@@ -0,0 +1,39 @@
+function abrir(){
+    document.getElementById("menu").classList.add("menuOpened");
+    document.getElementById("principal").style.width="80%";
+   
+    document.getElementById("open").style.display="block";
+    document.getElementById("closed").style.display="none";
+
+    var variavel = document.querySelectorAll('.funcao');
+    for (let i = 0; i < variavel.length; i++) {
+        variavel[i].style.display="block";
+    }
+
+    var circulo = document.querySelectorAll('.circle');
+    for (let i = 0; i < circulo.length; i++) {
+        circulo[i].style.width="17%";
+    }
+   
+    document.getElementById("chart").style.width="90%";
+}
+
+function fechar(){
+    document.getElementById("menu").classList.remove("menuOpened");
+    document.getElementById("principal").style.width="95%";
+    var variavel = document.querySelectorAll('.funcao');
+
+    document.getElementById("open").style.display="none";
+    document.getElementById("closed").style.display="block";
+
+    for (let i = 0; i < variavel.length; i++) {
+        variavel[i].style.display="none";
+    }
+
+    var circulo = document.querySelectorAll('.circle');
+    for (let i = 0; i < circulo.length; i++) {
+        circulo[i].style.width="15%";
+    }
+
+    document.getElementById("chart").style.width="100%";
+}
