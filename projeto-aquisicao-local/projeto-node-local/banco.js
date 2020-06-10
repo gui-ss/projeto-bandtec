d85857ea240c8724e9d22213755d0703723a47b9
gisele-araujo <gisele.flor@bandtec.com.br>
2020-06-10 16:54:21 -0300

    novo node yoshi

---
+++ projeto-aquisicao-local/projeto-node-local/banco.js
@@ -0,0 +1,33 @@
+const configuracoes = {
+    banco: {
+        server: "serverchameleon.database.windows.net",
+        user: "adminlocal",
+        password: "chameleon11!",
+        database: "bdchameleon1a",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    }
+}
+ 
+const sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+
+function conectar() {
+  sql.close();
+  return sql.connect(configuracoes.banco)
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
