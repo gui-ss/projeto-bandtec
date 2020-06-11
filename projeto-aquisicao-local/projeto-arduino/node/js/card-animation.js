b325f4d244dd650d74ab04b4c1575522bbf6b7ee
christian.raphael <christian.morais@bandtec.com.br>
2020-06-10 22:11:43 -0300

    Melhorias telas das salas

--- projeto-aquisicao-local/projeto-arduino/node/js/card-animation.js
+++ projeto-aquisicao-local/projeto-arduino/node/js/card-animation.js
@@ -1,34 +1,28 @@
 let cards = document.querySelectorAll(".box-graph-top");
-let arealeft = document.querySelector(".area-left");
-let arealefttop = document.querySelector(".top-graph");
 
-let bottom_graph = document.querySelector(".bottom-graph");
-let area_right = document.querySelector(".area-right");
+function expandCard(id) {
+    let card = document.querySelector(`#${id}`);
 
-function expandCard(e) {
-    if (e.target.classList.contains("box-graph-top")) {
-        let id = e.target.getAttribute("id");
+    let arealeft = card.querySelector(".area-left");
+    let arealefttop = card.querySelector(".top-graph");
+    let bottom_graph = card.querySelector(".bottom-graph");
+    let area_right = card.querySelector(".area-right");
 
 
-        cards.forEach(card => {
-            let cardId = card.getAttribute("id");
+    cards.forEach(card => {
+        let cardId = card.getAttribute("id");
 
-            if (cardId != id) {
-                card.classList.toggle('disabledCard');  
-                arealeft.classList.toggle('area-left-disabled');
-                arealefttop.classList.toggle('info-disabled');
-                bottom_graph.classList.toggle('none');
-                area_right.classList.toggle('none');
-            }
-            else {
-                setTimeout(function () {
-                    card.classList.toggle('expandedCard');
-                }, 10);
-            }
-        });
-    }
+        if (cardId != id) {
+            card.classList.toggle('disabledCard');
+            arealeft.classList.toggle('area-left-disabled');
+            arealefttop.classList.toggle('info-disabled');
+            bottom_graph.classList.toggle('none');
+            area_right.classList.toggle('none');
+        }
+        else {
+            setTimeout(function () {
+                card.classList.toggle('expandedCard');
+            }, 10);
+        }
+    });
 }
-
-cards.forEach(card => {
-    card.addEventListener('click', expandCard);
-});
