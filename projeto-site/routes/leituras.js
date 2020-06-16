ad56c594e1e8fe36749a0651e140a9cc0adc27a5
gisele-araujo <gisele.flor@bandtec.com.br>
2020-06-15 21:24:02 -0300

    update

--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -37,8 +37,8 @@ router.get('/tempo-real', function (req, res, next) {
 	
 	console.log(`Recuperando a última leitura`);
 
-	const instrucaoSql = `select top 1 temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico  
-						from leitura order by id desc`;
+	const instrucaoSql = `select top 1 temperatura, umidade, FORMAT(dataleitura,'HH:mm:ss') as momento_grafico  
+						from tbSensor order by idsensor desc`;
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
 		.then(resultado => {
